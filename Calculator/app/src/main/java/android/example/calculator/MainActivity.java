5bc176fa042f7ecbc60d6d78ce214d74cad0b522
Valeriya <valeriya.ridchenko@mail.ru>
2019-06-13 17:43:51 +0300

    new logic

--- Calculator/app/src/main/java/android/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/android/example/calculator/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnPlus, btnMinus, btnResult, btnDiv, btnMult, btnDel;
     TextView result;
     String[] varsArray;
+    ArrayList<Double> varsList = new ArrayList<Double>();
     ArrayList<String> operations = new ArrayList<String>();
     String mystr;
     String[] calcArray;
@@ -120,15 +121,101 @@ public class MainActivity extends AppCompatActivity {
                 mystr = mystr.replaceAll("[^0-9]", "_");
                 varsArray = mystr.split("_");
                 result.setText("");
-
+                Double[] vars = new Double[varsArray.length];
+                for(int i = 0; i < varsArray.length; i++){
+                    vars[i] = Double.parseDouble(varsArray[i]);
+                }
                 //Sort(operations,varsArray);
                 //result.append(Calculation(calcOper,calcArray));
 
-                result.append(Calculation(operations,varsArray));
+                //result.append(Calculation(operations,varsArray));
+                result.append(calcLogic(operations, vars).toString());
             }
 
         }
     };
+    private Double calcLogic(ArrayList<String> operat, Double[] varsArr){
+        String[] newArr = new String[varsArr.length-2];//уменьшаем исходный массив на 1, поэтому -2
+        double result = 0;
+        while(varsArr.length != 1) {
+            for (int i = 0; i < operat.size(); i++) {
+                if (operat.get(i) == "multiplication") {
+                    varsArr[i] = varsArr[i] * varsArr[i + 1];
+                    varsArr[i + 1] = 0.0;
+                    operat.set(i, "");
+                    varsArr = Shift(varsArr, operat, i + 1);
+                    operat = Shift2(varsArr, operat, operat.size(), i);
+                }
+            }
+            for (int i = 0; i < operat.size(); i++) {
+                if (operat.get(i) == "division") {
+                    varsArr[i] = varsArr[i] / varsArr[i + 1];
+                    varsArr[i + 1] = 0.0;
+                    operat.set(i, "");
+                    varsArr = Shift(varsArr, operat, i + 1);
+                    operat = Shift2(varsArr, operat, operat.size(), i);
+                }
+            }
+            if(operat.get(0) != ""){
+                //если убрать проверку, то будет исключение
+                for (int i = 0; i < operat.size(); i++) {
+                    if (operat.get(i) == "plus") {
+                        varsArr[i] = varsArr[i] + varsArr[i + 1];
+                        varsArr[i + 1] = 0.0;
+                        operat.set(i, "");
+                        varsArr = Shift(varsArr, operat, i + 1);
+                        operat = Shift2(varsArr, operat, operat.size(), i);
+                    }
+                    else {
+                        varsArr[i] = varsArr[i] - varsArr[i + 1];
+                        varsArr[i + 1] = 0.0;
+                        operat.set(i, "");
+                        varsArr = Shift(varsArr, operat, i + 1);
+                        operat = Shift2(varsArr, operat, operat.size(), i);
+                    }
+                }
+            }
+        }
+        return varsArr[0];
+    }
+    private Double[] Shift(Double[] varsArr, ArrayList<String> operat, int index) {
+        System.out.println("say hi");
+        Double[] newArr = new Double[varsArr.length-1];
+        if (index != 0) {
+            for (int i = index; i < varsArr.length - 1; i++) {
+                varsArr[i] = varsArr[i + 1];
+            }
+            for (int i = 0; i < newArr.length; i++){
+                newArr[i] = varsArr[i];
+            }
+            return newArr;
+        }
+        else
+            return varsArr;
+    }
+    private ArrayList<String> Shift2(Double[] varsArr, ArrayList<String> operat, int size, int index) {
+        //Double[] newArr = new Double[varsArr.length-1];
+            ArrayList<String> oper = new ArrayList<String>();
+            if(size != 1) {
+                //for (int i = index; i < size - 1; i++) {
+                //    if(i < oper.size())
+                //        oper.set(i, operat.get(i + 1));
+                //    else
+                //        oper.add(i, operat.get(i + 1));
+                //}
+                for(int i = index; i < operat.size()-1;i++){
+                    operat.set(i, operat.get(i+1));
+                }
+                for(int i = 0; i < operat.size()-1;i++){
+                    oper.add(operat.get(i));
+                }
+                return oper;
+            }
+            else{
+                oper.add(operat.set(size-1,""));
+            }
+            return oper;
+    }
     private String Calculate(ArrayList<String> operat, String[] varsArr){
         double result = 0;
         result = Double.parseDouble(varsArr[0]);
